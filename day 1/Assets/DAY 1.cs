061f985c2b795052cac9af4bbade9c48f6235017
hadeerale <Hadeeer2007@gmail.com>
2023-08-27 23:49:41 +0300

    DDone

---
+++ day 1/Assets/DAY 1.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DAY1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string heroName = "Tota";
+        
+        int heroAge = 33;
+        
+        float heroHeight = 177.9f;
+       
+        string heroSuperPower = "super spee";
+
+        
+        string villainName = "bad demon";
+        
+        int villainAge = 77;
+       
+        float villainHeight = 190f;
+        
+        string villainSuperPower = "laser Eyes shooting";
+
+
+        int agedifferene = 77 - 33;
+
+        string villainSuperPower = "none";
+
+
+        void Start ()
+        {
+
+            print(ageDifferene);
+            print(heroName + heroAge + heroHeight + heroSuperPower);
+            print(villainName + villainAge + villainHeight + villainSuperPower);
+            print(heroAge + 5);
+        }
+            
+           
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
